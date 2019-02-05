9e71259b7d7e8be7bf9588a274cac7fc68c54a67
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-05 14:32:24 +0200

    layout basic recipe-list view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.less']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]  = [
+    new Recipe('A test recipe', 'Simply a test',
+      'https://pinchofyum.com/wp-content/uploads/General-Tsos-Cauliflower-1-7-600x900.jpg'),
+    new Recipe('A test recipe', 'Simply a test',
+      'https://pinchofyum.com/wp-content/uploads/General-Tsos-Cauliflower-1-7-600x900.jpg'),
+    new Recipe('A test recipe', 'Simply a test',
+      'https://pinchofyum.com/wp-content/uploads/General-Tsos-Cauliflower-1-7-600x900.jpg')
+  ];
 
   constructor() { }
 
