5037372264fc5e55524ed7b4bff8d93849e99e26
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-10 22:30:45 +0200

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,28 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Simply a test',
-      'https://pinchofyum.com/wp-content/uploads/General-Tsos-Cauliflower-1-7-600x900.jpg'),
-    new Recipe('Another test recipe', 'Again a test',
-      'https://pinchofyum.com/wp-content/uploads/General-Tsos-Cauliflower-1-7-600x900.jpg'),
-    new Recipe('Third test recipe', 'Simply a test',
-      'https://pinchofyum.com/wp-content/uploads/General-Tsos-Cauliflower-1-7-600x900.jpg')
+    new Recipe('A test recipe',
+      'Simply a test',
+      'https://www.cityline.tv/wp-content/uploads/2017/12/salmon-and-vegetables-plate.jpg',
+      [new Ingredient('meat', 1),
+        new Ingredient('salad', 5)]
+    ),
+    new Recipe('Another test recipe',
+      'Again a test',
+      'https://www.cityline.tv/wp-content/uploads/2017/12/salmon-and-vegetables-plate.jpg',
+      [new Ingredient('tomatoes', 2),
+        new Ingredient('buns', 3)]),
+    new Recipe('Third test recipe',
+      'Simply a test',
+      'https://www.cityline.tv/wp-content/uploads/2017/12/salmon-and-vegetables-plate.jpg',
+      [new Ingredient('steak', 1),
+        new Ingredient('carrots', 5)])
   ];
 
   getRecipes(): Recipe[] {
