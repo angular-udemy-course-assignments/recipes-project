df9a97971c55dfc5fa166f185c7ac8220860df9a
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-24 15:43:03 +0200

    edit recipe - Validation for dynamic inputs

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -34,8 +34,9 @@ export class RecipeEditComponent implements OnInit {
   onAddIngredient() {
     (<FormArray>this.recipeForm.get('ingredients'))
       .push(new FormGroup({
-        'name': new FormControl(),
-        'amount': new FormControl()
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null,
+          [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       }));
   }
 
