edb3c1f2d7d4d936ff2cd7bb7d6a39c20828fd10
nkolchakov <nikola.kolchakov@gmail.com>
2019-03-06 00:04:16 +0200

    extract a Core Module containg the header and home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
 import {RecipeService} from './recipes/recipe.service';
 import {HttpModule} from '@angular/http';
 import {DataStorageService} from './shared/data-storage.service';
@@ -10,13 +9,11 @@ import {AuthGuardService} from './auth/auth-guard.service';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +21,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     SharedModule,
     AuthModule,
-    HttpModule
+    HttpModule,
+    CoreModule
   ],
   providers: [RecipeService, DataStorageService, AuthGuardService],
   bootstrap: [AppComponent]
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
-import {DataStorageService} from '../shared/data-storage.service';
-import {RecipeService} from '../recipes/recipe.service';
-import {AuthService} from '../auth/auth.service';
+import {DataStorageService} from '../../shared/data-storage.service';
+import {RecipeService} from '../../recipes/recipe.service';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
